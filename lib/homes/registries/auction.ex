aaaa49a0d2b0aba619ff9db13b9392c16dface60
Nato Szmedra <nato@producteca.com>
2020-06-27 20:05:59 -0300

    define auction registry as module based

---
+++ lib/homes/registries/auction.ex
@@ -0,0 +1,21 @@
+defmodule AuctionRegistry do
+  use Horde.Registry
+
+  def init(options) do
+    {:ok, Keyword.put(options, :members, get_members())}
+  end
+
+  def start do
+    Horde.Registry.start_link(keys: :unique, name: AuctionRegistry)
+  end
+
+  def auctionById(id) do
+    Horde.Registry.lookup(AuctionRegistry, String.to_integer(id))
+      |> List.first
+  end
+
+  defp get_members() do
+    [Node.self() | Node.list()]
+    |> Enum.map(fn node -> {AuctionRegistry, node} end)
+  end
+end
